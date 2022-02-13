32ff42321ea583311ed4bc6947ca729fb4d9a649
danh.dong-xuan <danh.dong-xuan@banvien.com.vn>
2022-02-13 09:09:18 +0700

    add branch of xuan thach

--- src/com/org/java/demo.java
+++ src/com/org/java/demo.java
@@ -1,8 +1,23 @@
 package com.org.java;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 public class demo {
     public static void main(String[] args) {
         System.out.println("hello xuan danh");
+
+        String path = "c:\\projects\\app.log";
+
+        try {
+
+            // default StandardCharsets.UTF_8
+            String content = Files.readString(Paths.get(path));
+            System.out.println(content);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public void setAge(int age){
